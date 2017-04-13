c3006e690c7de77a6b129fedeea8ece010ad2fde
zhou <634975529@qq.com>
2017-04-13 20:01:56 +0800

    这是dev的第二次分支的提交

--- src/main/java/com/ucai/property_animatorset/MainActivity.java
+++ src/main/java/com/ucai/property_animatorset/MainActivity.java
@@ -63,6 +63,8 @@ public class MainActivity extends AppCompatActivity {
         //我正在分支dev中工作
         //我修改好了代码。
         //要准备在mater主分支中合并代码了
+        //fjlksadjf;jsad
+        //dsfjasdfj
 
     }
 
